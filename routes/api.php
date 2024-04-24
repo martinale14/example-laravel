2c20218fb354f7d78f0c4a2e252f3f14395978a0
Martin Alejandro Escobar Espinel <martinale1401@gmail.com>
2024-04-24 16:01:57 -0500

    change route

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/user', [UserController::class, "store"]);
+Route::post('/users', [UserController::class, "store"]);
